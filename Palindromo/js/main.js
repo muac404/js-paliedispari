b280a51ee3abd9083265a99a4e1d56fe0a2dbdf7
muac404 <muac404@gmail.com>
2022-10-30 12:35:50 +0100

    Aggiunge js verifica parola palindroma

--- Palindromo/js/main.js
+++ Palindromo/js/main.js
@@ -1,1 +1,29 @@
 "use strict"
+
+const word = prompt("Inserisci una parola");
+
+let wordLR = "";
+
+for (let i = 0; i < word.length; i++) {
+
+    wordLR += word[i];
+    
+}
+
+console.log("Leggo da sinistra verso destra =", wordLR)
+
+let wordRL = "";
+
+for (let i = word.length -1; i >= 0; i--) {
+
+    wordRL += word[i];
+    
+}
+
+console.log("Leggo da da destra verso sinistra =", wordRL)
+
+if(wordLR == wordRL){
+    alert(`La parola " ${word} " è palindroma`)
+} else{
+    alert(`La parola " ${word} " non è palindroma`)
+}
