548c2191ff39b4b2375c993f82bf9aa111107ca6
muac404 <muac404@gmail.com>
2022-11-02 10:32:09 +0100

    corregge function

--- Palindromo/js/main.js
+++ Palindromo/js/main.js
@@ -1,29 +1,53 @@
 "use strict"
-const userName = prompt("Come ti chiami?")
-const word = prompt("Inserisci una parola");
+// const userName = prompt("Come ti chiami?")
+// const word = prompt("Inserisci una parola");
 
-let wordLR = "";
+// let wordLR = "";
 
-for (let i = 0; i < word.length; i++) {
+// for (let i = 0; i < word.length; i++) {
 
-    wordLR += word[i];
+//     wordLR += word[i];
     
-}
+// }
 
-console.log("Leggo da sinistra verso destra =", wordLR)
+// console.log("Leggo da sinistra verso destra =", wordLR)
 
-let wordRL = "";
+// let wordRL = "";
 
-for (let i = word.length -1; i >= 0; i--) {
+// for (let i = word.length -1; i >= 0; i--) {
 
-    wordRL += word[i];
+//     wordRL += word[i];
     
+// }
+
+// console.log("Leggo da destra verso sinistra =", wordRL)
+
+// if(wordLR == wordRL){
+//     alert(`Ciao ${userName}, la parola " ${word} " è palindroma`)
+// } else{
+//     alert(`Ciao ${userName}, la parola " ${word} " non è palindroma`)
+// }
+
+function isPalindrome(word){
+    let wordReverse = "";
+    for (let i = word.length - 1;  i >= 0; i--) {
+        wordReverse = wordReverse + word[i];
+    }
+
+    
+    if ( word.toLowerCase() === wordReverse.toLowerCase() ){
+        return true;
+    }
+
+    return false;
 }
 
-console.log("Leggo da destra verso sinistra =", wordRL)
-
-if(wordLR == wordRL){
-    alert(`Ciao ${userName}, la parola " ${word} " è palindroma`)
-} else{
-    alert(`Ciao ${userName}, la parola " ${word} " non è palindroma`)
+if( isPalindrome("osso")){
+    alert("La parola è palindorma")
+} else {
+    alert("La parola non è palindroma")
 }
+
+
+
+isPalindrome(word);
